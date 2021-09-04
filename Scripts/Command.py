a1bc52f8a8b9ec61a1714198b11bd1c81fbf73db
Reversible Card <78249745+Lighthigh57@users.noreply.github.com>
2021-09-05 08:01:23 +0900

    Use_Command

---
+++ Scripts/Command.py
@@ -0,0 +1,13 @@
+import CHaser
+
+class Command:
+    run = None
+    info = None
+    def __init__(self,runner):
+        run = runner
+
+    def move(self, com,dir):
+        """
+        docstring
+        """
+        pass
