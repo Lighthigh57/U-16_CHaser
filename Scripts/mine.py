1859982c36be0a66fd97bb0aa30298cafcd7b98e
Reversible Card <78249745+Lighthigh57@users.noreply.github.com>
2021-09-05 08:46:03 +0900

    mine_Changed

--- Scripts/mine.py
+++ Scripts/mine.py
@@ -1,61 +1,33 @@
-import CHaser # 同じディレクトリに CHaser.py がある前提
 import random
 import Command
 
-value = [] # フィールド情報を保存するリスト
-client = None
+ready_value = [] # フィールド情報を保存するリスト
+runner = None
 
 
 def main():
-    global value
-    global client
+    global ready_value
+    global runner
     while(True):
-        value = client.get_ready()
+        ready_value = runner.get_info()
         for i in range(1,8,2):
-            if value[i] == 1:
-                command(i)
+            if ready_value[i] == 1:
+                runner.move("put",i)
                 break
         else:
             check=True
             while check:
                 result = random.randint(0,3)
-                if value[result] != 2:
-                    command(result*2+1)
+                if ready_value[result] != 2:
+                    runner.move("walk",result*2+1)
                     check = False
 
 
-def command(target):
 
-    """
-    位置に合わせて関数実行
-    """
-    global value
-    global client
-    if target == 1:
-        if value[target] == 1:
-            client.put_up()
-        else:
-            client.walk_up()
-    if target == 3:
-        if value[target] == 1:
-            client.put_left()
-        else:
-            client.walk_left()
-    if target == 5:
-        if value[target] == 1:
-            client.put_right()
-        else:
-            client.walk_right()
-    if target == 7:
-        if value[target] == 1:
-            client.put_down()
-        else:
-            client.walk_down()
 """
 python sample.py のようにこのファイルを直接実行すると，
 __name__ は "__main__" となる．これを利用して main() を実行する．
 """
 if __name__ == "__main__":
-    client = CHaser.Client()
-    
+    runner = Command.Command()
     main()
