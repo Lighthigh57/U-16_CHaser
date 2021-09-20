171f83fafd89bca7c6ad25b5cfe000cddf83467b
Reversible Card <78249745+Lighthigh57@users.noreply.github.com>
2021-09-20 21:25:42 +0900

    Left_hand repeatnow

--- Scripts/Go_Light.py
+++ Scripts/Go_Light.py
@@ -1,34 +1,69 @@
-import random
 import Command
 
 ready_value = [] # フィールド情報を保存するリスト
+priority = [0,0,0,0,0,0,0,0,0] #
 runner = None
 
 
 def main():
     global ready_value
     global runner
+    global priority
     while True:
         ready_value = runner.get_info()
-        rival_Checker()
-    
+        for i in range(0,9):
+            priority[i] = 0
+        Checker()
 
-def rival_Checker():
+def avoid_enemy(target):
+    """
+    敵から逃げるには?
+    """
+    global priority
+
+    if target == 0:
+        priority[1] = -1
+        priority[3] = -1
+    else:
+        if target == 2:
+            priority[1] = -1
+            priority[5] = -1
+        else:
+            if target == 6:
+                priority[3] = -1
+                priority[7] = -1
+            else:
+                priority[5] = -1
+                priority[7] = -1    
+
+def Checker():
     """
     敵いたら潰します(笑)
     """
     global ready_value
     global runner
+    global priority
 
     for i in range(0,9):
-        if ready_value[i] == 1:
+        if ready_value[i] == 3:
+            if i%2==1:
+                priority[i] = 1
+        elif ready_value[i] == 1:
             if i%2==0:
-                pass
+                avoid_enemy(i)
             else:
                 runner.move("put",i)
                 break
+        elif ready_value[i] == 2:
+            priority[i] = -1
+            pass
     else:
-        runner.move("look",1)
+        max = 1
+        for i in range(3,8,2):
+            if priority[max] < priority[i]:
+                max = i
+        runner.move("walk",max)
+    
     
     
 
